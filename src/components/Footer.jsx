6c99aef317258c0392d3b7b1015e46d2b40149c4
Souvik Pal <84220139+g8ed-vik@users.noreply.github.com>
2023-06-17 19:39:41 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-primary py-16">
+      <div className="container mx-auto">
+        <p className="text-center text-white">
+          Copyright &copy; FakeStore 2023. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
